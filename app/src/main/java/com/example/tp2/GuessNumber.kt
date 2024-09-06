2db06705bc4aa38a7202cd086f2a07b9e7fc417b
Maximo <maxipreneste@gmail.com>
2024-09-06 00:41:20 -0300

    Incremento puntos pero no decremento intentos

--- app/src/main/java/com/example/tp2/GuessNumber.kt
+++ app/src/main/java/com/example/tp2/GuessNumber.kt
@@ -7,13 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlin.random.Random
-
+import kotlin.random.nextInt
 
 class GuessNumber : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,27 +30,43 @@ class GuessNumber : ComponentActivity() {
         setContent {// dentro de estas llaves se pinta en la pantalla del telefono
             //se pueden llamar las funciones que tengan la etiqueta de composable
             //ViewModel - State
-            Column(
-                Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Adivina")
-                for (i in 1..5) {
-                    Number(i)
+            RandomNumberScreen()
+        }
+    }
+    @Composable
+    fun RandomNumberScreen(){
+        var score by remember {
+            mutableIntStateOf(0)
+        }
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            ScoreGame(score = score)
+            for (i in 1..5) {
+                Number(i){
+                    score += 10
                 }
             }
         }
     }
-
     @Composable
-    fun Number(number: Int) {
+    fun Number(number: Int, onClick:()->Unit) {
         Button(onClick = {
-            if (number == Random.nextInt(1, 5)) {
-                println("trolo")
+            if (number == Random.nextInt(1..5)) {
+                onClick()
             }
-        }) {
+            }) {
             Text(text = number.toString())
+        }
+    }
+    @Composable
+    fun ScoreGame (score: Int){
+        Card() {
+            Text(text = score.toString(),
+                fontSize = 30.sp,
+                modifier = Modifier.padding(8.dp))
         }
     }
 }
