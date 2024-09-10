52f007d089cc0611fd21fce710a0ed905991bde4
MorettiFrancisco <franmoretti02@hotmail.com>
2024-09-09 21:20:47 -0300

    Model and database classes added

--- app/src/main/java/com/example/tp2/Capital.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tp2
-
-data class Capital(
-    val countryName: String,
-    val capitalName: String,
-    val population: Int
-)
---
+++ app/src/main/java/com/example/tp2/Data/Capital.kt
@@ -0,0 +1,8 @@
+package com.example.tp2
+
+data class Capital(
+    val id: Long = -1,
+    val nombrePais: String,
+    val nombreCapital: String,
+    val habitantesPromedio: Int
+)
