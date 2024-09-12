9192881ed077b5eaafdf0b9448be8c75784e8f0a
MorettiFrancisco <franmoretti02@hotmail.com>
2024-09-12 18:30:43 -0300

    esto me funciona

--- app/src/main/java/com/example/tp2/GameViewModel.kt
+++ app/src/main/java/com/example/tp2/GameViewModel.kt
@@ -1,40 +1,44 @@
 package com.example.tp2
 
+import android.app.Application
+import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 import kotlin.random.nextInt
 
-class GameViewModel:ViewModel() {
-    var gameState by mutableStateOf(GameState())
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+    private val sharedPreferences = application.getSharedPreferences("game_prefs", Context.MODE_PRIVATE)
+    var gameState by mutableStateOf(GameState(bestScore = sharedPreferences.getInt("best_score", 0)))
         private set
+
     init {
         viewModelScope.launch {  }
-
     }
-    fun onClickNumber(number:Int){
-        if (number== Random.nextInt(1..5)){
-                val prevScore:Int=gameState.score
-                gameState=gameState.copy(score =prevScore+10 )
-        }else{
-               manageAttempts()
 
+    fun onClickNumber(number: Int) {
+        if (number == Random.nextInt(1..5)) {
+            val prevScore: Int = gameState.score
+            gameState = gameState.copy(score = prevScore + 10)
+        } else {
+            manageAttempts()
         }
-        if (gameState.bestScore<gameState.score){
-            gameState=gameState.copy(bestScore = gameState.score)
+        if (gameState.bestScore < gameState.score) {
+            gameState = gameState.copy(bestScore = gameState.score)
+            sharedPreferences.edit().putInt("best_score", gameState.bestScore).apply()
         }
     }
-    private fun manageAttempts(){
-        val prevAttemps:Int=gameState.attempts
-        gameState=gameState.copy(attempts = prevAttemps-1)
-        if (gameState.attempts<0){
-            gameState=gameState.copy(score = 0,
-                attempts = 5)
+
+    private fun manageAttempts() {
+        val prevAttempts: Int = gameState.attempts
+        gameState = gameState.copy(attempts = prevAttempts - 1)
+        if (gameState.attempts < 0) {
+            gameState = gameState.copy(score = 0, attempts = 5)
         }
     }
 }
